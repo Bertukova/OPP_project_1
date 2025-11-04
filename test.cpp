f15f5ed629427a66b1de18b03c5988ee08b221af
olesya-kk <olesya_kostikova@bk.ru>
2025-11-04 21:23:15 +0300

    Тест 1. Убедиться, что одновременно может курить только один курильщик. Убедиться, что механизм блокировки работает правильно.

--- test.cpp
+++ test.cpp
@@ -25,5 +25,35 @@ protected:
     std::mutex test_mutex;
 };
 
-
+// Тест 1: Только один курильщик за раунд
+TEST_F(SmokingTableTest, OnlyOneSmokerPerRound) {
+    std::atomic<int> active_smokers{0};
+    std::atomic<bool> test_running{true};
+    
+    auto smoker_task = [&](Ingredient ingredient) {
+        if (table->startSmoking(ingredient)) {
+            active_smokers++;
+            while (test_running) {
+                std::this_thread::sleep_for(std::chrono::milliseconds(10));
+            }
+            table->finishSmoking();
+        }
+    };
+    
+    std::thread smoker1(smoker_task, Ingredient::kTobacco);
+    std::thread smoker2(smoker_task, Ingredient::kPaper);
+    std::thread smoker3(smoker_task, Ingredient::kMatches);
+    
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    table->place(Ingredient::kPaper, Ingredient::kMatches);
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    
+    EXPECT_EQ(active_smokers.load(), 1);
+    
+    test_running = false;
+    table->finish();
+    smoker1.join();
+    smoker2.join();
+    smoker3.join();
+}
 
